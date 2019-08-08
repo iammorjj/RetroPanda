dc52f6de13c2d068160a9424bd58d2c832ad5ccd
Alexander Mordovsky <alexandermordovsky@Alexanders-MacBook-Air.local>
2019-08-08 18:39:34 -0400

    better refactor to conv and burgers

---
+++ MyGame/Conveyor.cpp
@@ -0,0 +1,9 @@
+//
+//  Conveyor.cpp
+//  MyGame
+//
+//  Created by Alexander Mordovsky on 08/08/2019.
+//  Copyright © 2019 Alexander Mordovsky. All rights reserved.
+//
+
+#include "Conveyor.hpp"
---
+++ MyGame/Conveyor.hpp
@@ -0,0 +1,14 @@
+//
+//  Conveyor.hpp
+//  MyGame
+//
+//  Created by Alexander Mordovsky on 08/08/2019.
+//  Copyright © 2019 Alexander Mordovsky. All rights reserved.
+//
+
+#ifndef Conveyor_hpp
+#define Conveyor_hpp
+
+#include <stdio.h>
+
+#endif /* Conveyor_hpp */
