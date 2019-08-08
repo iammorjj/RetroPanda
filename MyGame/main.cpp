39d313c9b4651182fd3117380ebd9b0d086770c1
Alexander Mordovsky <alexandermordovsky@Alexanders-MacBook-Air.local>
2019-08-08 03:18:29 -0400

    burger frame added, some refactoring

---
+++ MyGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MyGame
+//
+//  Created by Alexander Mordovsky on 08/08/2019.
+//  Copyright © 2019 Alexander Mordovsky. All rights reserved.
+//
+
+#include "CApp.hpp"
+
+int main(int argc, char* argv[]) {
+    CApp theApp;
+    
+    return theApp.OnExecute();
+}
