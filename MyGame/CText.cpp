710ffa3055598f3fa54e66eb5f5c8f9709afd2c4
Alexander Mordovsky <alexandermordovsky@Alexanders-MacBook-Air.local>
2019-08-20 03:33:04 -0400

    best score

---
+++ MyGame/CText.cpp
@@ -0,0 +1,9 @@
+//
+//  CText.cpp
+//  MyGame
+//
+//  Created by Alexander Mordovsky on 20/08/2019.
+//  Copyright © 2019 Alexander Mordovsky. All rights reserved.
+//
+
+#include "CText.hpp"
---
+++ MyGame/CText.hpp
@@ -0,0 +1,14 @@
+//
+//  CText.hpp
+//  MyGame
+//
+//  Created by Alexander Mordovsky on 20/08/2019.
+//  Copyright © 2019 Alexander Mordovsky. All rights reserved.
+//
+
+#ifndef CText_hpp
+#define CText_hpp
+
+#include <stdio.h>
+
+#endif /* CText_hpp */
