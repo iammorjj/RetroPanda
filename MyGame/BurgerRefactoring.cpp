c993b5775ea35881d5f759f4608d81fad0811bd9
MacAir AM <iammorjj@gmail.com>
2019-09-17 19:49:35 +0300

    removed old cpp

--- MyGame/BurgerRefactoring.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  BurgerRefactoring.cpp
-//  MyGame
-//
-//  Created by Alexander Mordovsky on 02/09/2019.
-//  Copyright © 2019 Alexander Mordovsky. All rights reserved.
-//
-
-#include <stdio.h>
